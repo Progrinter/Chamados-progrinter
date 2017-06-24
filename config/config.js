6bf59e3f39832e1b73fbcf93d7595206d2b7faa3
GCostaSM <gabriel.mei@uol.com.br>
2017-06-24 02:08:05 -0300

    make it better

--- config/config.js
+++ config/config.js
@@ -1,11 +1,13 @@
 var express = require(`express`);
 var consign = require('consign');
-var http = require('http').Server(app);
+
 
 
 module.exports = function(){
     var app = express();
+
     
+
     app.set("view engine","ejs")
     app.set("views","./views");
     app.use(express.static('./resources'));
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var app = require('./config/config')();
 
 
-
+var http = require('http').Server(app);
 
 http.listen(process.env.PORT || 8080, function(){
   console.log('listening on', process.env.PORT|| 8080);
