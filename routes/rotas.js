b55ea30c6ccf1e29d2d88ce248df3deba9ccf158
GCostaSM <gabriel.mei@uol.com.br>
2017-06-24 15:53:39 -0300

    finalização da criação de telas

--- routes/rotas.js
+++ routes/rotas.js
@@ -12,7 +12,10 @@ module.exports = function(app){
         res.render("solicitacao.ejs");
     });
     
-
+    app.get('/login',function(req,res){
+        res.render("login.ejs");
+    });
+    
 
     app.get('/home',function(req,res){
         res.render("home.ejs");
